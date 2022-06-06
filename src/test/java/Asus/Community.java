c31d6687d99145abbc8c54bd3cfc25c93622be56
SpideAmg <amghardzamghar@gmail.com>
2022-06-05 20:38:05 -0400

    AsusFirstPush

---
+++ src/test/java/Asus/Community.java
@@ -0,0 +1,17 @@
+package Asus;
+
+import Base.CommonAPI;
+import org.testng.annotations.Test;
+
+public class Community extends CommonAPI {
+    @Test
+    public void Com(){
+        click("#cookie-policy-info > div > div.cookie-btn-box > div.btn-asus.btn-ok.btn-read-ck");
+        click("#OfficialFooterWrapper > div:nth-child(1) > div.Footer__footerRightWrapper__GbL7a > div > ul > li:nth-child(1) > a");
+        click("#OfficialFooterWrapper > div:nth-child(1) > div.Footer__footerRightWrapper__GbL7a > div > ul > li:nth-child(2) > a");
+        click("#OfficialFooterWrapper > div:nth-child(1) > div.Footer__footerRightWrapper__GbL7a > div > ul > li:nth-child(3) > a");
+        click("#OfficialFooterWrapper > div:nth-child(1) > div.Footer__footerRightWrapper__GbL7a > div > ul > li:nth-child(4) > a");
+        click("#OfficialFooterWrapper > div:nth-child(1) > div.Footer__footerRightWrapper__GbL7a > div > ul > li:nth-child(6) > a");
+
+    }
+}
---
+++ src/test/java/Asus/FindDriver.java
@@ -0,0 +1,17 @@
+package Asus;
+
+import Base.CommonAPI;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class FindDriver extends CommonAPI {
+    @Test
+    public void FDriver() {
+        click("#cookie-policy-info > div > div.cookie-btn-box > div.btn-asus.btn-ok.btn-read-ck");
+        hoverOver("#wrapperDesktop > div.HeaderDesktop__headerLayerTwo__356nX > div > div.HeaderDesktop__navContainer__2TkWh > div > div.HeaderDesktop__secondRightMenuItem__3JfQe.HeaderDesktop__themeWhite__1bpoN.HeaderDesktop__hasBottomLine__3fHEX");
+        click("#wrapperDesktop > div.HeaderDesktop__headerLayerTwo__356nX > div > div.HeaderDesktop__navContainer__2TkWh > div > div.HeaderDesktop__secondRightMenuItem__3JfQe.HeaderDesktop__themeWhite__1bpoN.HeaderDesktop__hasBottomLine__3fHEX > div > div > a:nth-child(1)");
+        click("/html/body/div[2]/main/div[4]/div[2]/div[1]");
+        click("#topBanner > div > div > div.bottom-wrapper > ul > li:nth-child(4) > div > span > svg");
+
+    }
+}
